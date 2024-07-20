b646d0e45769639c7ff9e591e1cac074b820ecd9
Angelle <Angelles@users.noreply.github.com>
2024-07-20 17:11:13 -0400

    Add try/catch blocks to server actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -23,14 +23,21 @@ export async function createInvoice(formData: FormData) {
       });
       const amountInCents = amount * 100; // avoid floating-point errors
       const date = new Date().toISOString().split('T')[0]; // invoice creation date in MM-DD-YYYY format
+      
+    try {
       await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-
-    revalidatePath('/dashboard/invoices'); // clear cache and trigger request to the server
-    redirect('/dashboard/invoices'); // redirect user back to the invoices page
 }
+ catch (error){
+  return{
+    message: 'Database Error: Failure to create invoice',
+  };
+}
+revalidatePath('/dashboard/invoices'); // clear cache and trigger request to the server
+    redirect('/dashboard/invoices'); // redirect user back to the invoices page
+ }
 
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
@@ -46,17 +53,29 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
+  try{
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
  
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+} catch (error){
+  return {
+    message: 'Database Error: Unable to update invoice',
+  };
+}
+revalidatePath('/dashboard/invoices');
+redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+  try{
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+  } catch (error){
+    return{
+      message: 'Database Error: Unable to delete invoice',
+    };
+  }
 }
