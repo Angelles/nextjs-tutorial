8147ebb85b2efed24755649d173419fcd8636dac
Angelle <Angelles@users.noreply.github.com>
2024-07-20 16:55:45 -0400

    Add ability to edit an invoice and return the edited invoice info.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,3 +31,27 @@ export async function createInvoice(formData: FormData) {
     revalidatePath('/dashboard/invoices'); // clear cache and trigger request to the server
     redirect('/dashboard/invoices'); // redirect user back to the invoices page
 }
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
