6efac77e458f82ff6f06a4bafe07fba55da138f5
Angelle <Angelles@users.noreply.github.com>
2024-07-21 11:01:57 -0400

    Added errors. Investigate why Zod/state errors aren't showing up.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,39 +5,64 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+
  
 const FormSchema = z.object({
-  id: z.string(),
+  id: z.string({
+    required_error: 'Please select a customer.',
+  }),
   customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    required_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
- 
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
       });
-      const amountInCents = amount * 100; // avoid floating-point errors
-      const date = new Date().toISOString().split('T')[0]; // invoice creation date in MM-DD-YYYY format
+       // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      };
+    }
+
+    // Prepare data for db insertion
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100; // avoid floating-point errors
+    const date = new Date().toISOString().split('T')[0]; // invoice creation date in MM-DD-YYYY format
       
-    try {
+  try {
       await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+  }
+  catch (error){
+    return{
+      message: 'Database Error: Failure to create invoice',
+    };
+  }
+  revalidatePath('/dashboard/invoices'); // clear cache and trigger request to the server
+  redirect('/dashboard/invoices'); // redirect user back to the invoices page
 }
- catch (error){
-  return{
-    message: 'Database Error: Failure to create invoice',
-  };
-}
-revalidatePath('/dashboard/invoices'); // clear cache and trigger request to the server
-    redirect('/dashboard/invoices'); // redirect user back to the invoices page
- }
 
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
