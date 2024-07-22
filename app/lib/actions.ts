bbc4f45f5e4db533854c90dee1da3f0219635919
Angelle <Angelles@users.noreply.github.com>
2024-07-22 14:49:18 -0400

    Update import statement for auth again

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,7 +6,7 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { useFormState } from 'react-dom';
-import { signIn } from '@/auth';
+import { signIn } from '@/app/auth';
 import { AuthError } from 'next-auth';
 
 export async function authenticate(
